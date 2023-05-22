f3c4db6d206525d3fc002d74be3dc681b4502879
Jack Roddy <jack.w.roddy@gmail.com>
2023-05-22 11:43:20 -0600

    fixed an mmseqs parameter

--- scripts/run-mmseqs.sh
+++ scripts/run-mmseqs.sh
@@ -16,13 +16,14 @@ SORTED=$DIR/results.sorted
 
 EVALUE_COL=7
 
+rm -rf $DIR
 mkdir -p $DIR
 mkdir $PREP
 
 mmseqs convertmsa $QUERY $MSA_DB --identifier-field 0
 mmseqs msa2profile $MSA_DB $QUERY_DB --match-mode 1
 mmseqs createdb $TARGET $TARGET_DB
-mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB $PREP -s 7.5
+mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB $PREP -s 7.5 --max-seqs 1000
 mmseqs convertalis $QUERY_DB $TARGET_DB $ALIGN_DB $OUT --format-output "target,query,tstart,tend,qstart,qend,evalue"
 
 sort -g -k$EVALUE_COL $OUT > $SORTED
