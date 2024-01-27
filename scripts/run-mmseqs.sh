b61f4d960f74ff74a4520535139644e77b46460d
jack <jack.w.roddy@gmail.com>
2024-01-27 10:48:55 -0700

    updated scripts

--- scripts/run-mmseqs.sh
+++ scripts/run-mmseqs.sh
@@ -14,59 +14,106 @@ QUERY=$BENCHMARK_DIR/$NAME.train.msa
 
 RESULTS_DIR=$BENCHMARK_DIR/results/mmseqs/
 
-TIME_DEFAULT_1=$RESULTS_DIR/mmseqs.default.1.time
-TIME_DEFAULT_8=$RESULTS_DIR/mmseqs.default.8.time
-
-TIME_SENSITIVE_1=$RESULTS_DIR/mmseqs.sensitive.1.time
-TIME_SENSITIVE_8=$RESULTS_DIR/mmseqs.sensitive.8.time
-
-TIME_PREFILTER_NAIL_1=$RESULTS_DIR/mmseqs.prefilter.nail.1.time
-TIME_PREFILTER_NAIL_8=$RESULTS_DIR/mmseqs.prefilter.nail.8.time
-
-TIME_ALIGN_NAIL_1=$RESULTS_DIR/mmseqs.align.nail.1.time
-TIME_ALIGN_NAIL_8=$RESULTS_DIR/mmseqs.align.nail.8.time
+TIME_DEFAULT=$RESULTS_DIR/mmseqs.default.time
+TIME_SENSITIVE=$RESULTS_DIR/mmseqs.sensitive.time
+TIME_NAIL=$RESULTS_DIR/mmseqs.nail.time
 
 PREP=$RESULTS_DIR/prep/
 TARGET_DB=$PREP/targetDb
 MSA_DB=$PREP/msaDb
 QUERY_DB=$PREP/queryDb
 
-K_NAIL=0
+THREADS=8
+
+# K_DEFAULT=6
+# K_SENSITIVE=6
+# MAX_SEQS_SENSITIVE=1000
+
+K_NAIL=6
 K_SCORE_NAIL=80
 MIN_UNGAPPED_SCORE_NAIL=15
 MAX_SEQS_NAIL=1000
 
-PREFILTER_DB_NAIL=$PREP/prefilterDb-nail
-
-ALIGN_DB_DEFAULT_1=$PREP/alignDb-default-1
-ALIGN_DB_DEFAULT_8=$PREP/alignDb-default-8
-ALIGN_DB_SENSITIVE_1=$PREP/alignDb-sensitive-1
-ALIGN_DB_SENSITIVE_8=$PREP/alignDb-sensitive-8
-ALIGN_DB_NAIL_1=$PREP/alignDb-nail-1
-ALIGN_DB_NAIL_8=$PREP/alignDb-nail-8
+ALIGN_DB_DEFAULT=$PREP/alignDb-default
+ALIGN_DB_SENSITIVE=$PREP/alignDb-sensitive
+ALIGN_DB_NAIL=$PREP/alignDb-nail
  
 OUT_DEFAULT=$RESULTS_DIR/mmseqs.default.tsv
 OUT_SENSITIVE=$RESULTS_DIR/mmseqs.sensitive.tsv
 OUT_NAIL=$RESULTS_DIR/mmseqs.nail.tsv
 
+STDOUT=$RESULTS_DIR/tmp
+
 rm -rf $RESULTS_DIR
 mkdir -p $RESULTS_DIR
 mkdir $PREP
 
-mmseqs convertmsa $QUERY $MSA_DB --identifier-field 0 > /dev/null
-mmseqs msa2profile $MSA_DB $QUERY_DB --match-mode 1 > /dev/null
-mmseqs createdb $TARGET $TARGET_DB > /dev/null
+echo "preparing files..."
+mmseqs convertmsa $QUERY $MSA_DB \
+    --identifier-field 0 \
+    > /dev/null
 
-/usr/bin/time -p -o $TIME_DEFAULT_1 mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB_DEFAULT_1 $PREP --threads 1 -e $E > /dev/null
-/usr/bin/time -p -o $TIME_DEFAULT_8 mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB_DEFAULT_8 $PREP --threads 8 -e $E > /dev/null
-mmseqs convertalis $QUERY_DB $TARGET_DB $ALIGN_DB_DEFAULT_1 $OUT_DEFAULT --format-output "target,query,tstart,tend,qstart,qend,evalue" > /dev/null
+mmseqs msa2profile $MSA_DB $QUERY_DB \
+    --match-mode 1 \
+    > /dev/null
 
-/usr/bin/time -p -o $TIME_SENSITIVE_1 mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB_SENSITIVE_1 $PREP --threads 1 -s 7.5 --max-seqs 1000 -e $E > /dev/null
-/usr/bin/time -p -o $TIME_SENSITIVE_8 mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB_SENSITIVE_8 $PREP --threads 8 -s 7.5 --max-seqs 1000 -e $E > /dev/null
-mmseqs convertalis $QUERY_DB $TARGET_DB $ALIGN_DB_SENSITIVE_1 $OUT_SENSITIVE --format-output "target,query,tstart,tend,qstart,qend,evalue" > /dev/null
+mmseqs createdb $TARGET $TARGET_DB \
+    > /dev/null
 
-/usr/bin/time -p -o $TIME_PREFILTER_NAIL_1 mmseqs prefilter $QUERY_DB $TARGET_DB $PREFILTER_DB_NAIL --threads 1 -k $K_NAIL --k-score $K_SCORE_NAIL --min-ungapped-score $MIN_UNGAPPED_SCORE_NAIL --max-seqs $MAX_SEQS_NAIL > /dev/null
-/usr/bin/time -p -o $TIME_PREFILTER_NAIL_8 mmseqs prefilter $QUERY_DB $TARGET_DB $PREFILTER_DB_NAIL --threads 8 -k $K_NAIL --k-score $K_SCORE_NAIL --min-ungapped-score $MIN_UNGAPPED_SCORE_NAIL --max-seqs $MAX_SEQS_NAIL > /dev/null
-/usr/bin/time -p -o $TIME_ALIGN_NAIL_1 mmseqs align $QUERY_DB $TARGET_DB $PREFILTER_DB_NAIL $ALIGN_DB_NAIL_1 --threads 1 -e $E > /dev/null
-/usr/bin/time -p -o $TIME_ALIGN_NAIL_8 mmseqs align $QUERY_DB $TARGET_DB $PREFILTER_DB_NAIL $ALIGN_DB_NAIL_8 --threads 8 -e $E > /dev/null
-mmseqs convertalis $QUERY_DB $TARGET_DB $ALIGN_DB_NAIL_1 $OUT_NAIL --format-output "target,query,tstart,tend,qstart,qend,evalue" > /dev/null
+echo "running mmseqs default..."
+/usr/bin/time -p -o $TIME_DEFAULT \
+    mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB_DEFAULT $PREP \
+    --threads $THREADS \
+    -e $E \
+    > $STDOUT
+
+awk '/real/ {print "time:", $2}' $TIME_DEFAULT
+grep "Index table k-mer threshold:" $STDOUT
+grep "k-mer similarity threshold: " $STDOUT
+echo
+
+mmseqs convertalis $QUERY_DB $TARGET_DB $ALIGN_DB_DEFAULT $OUT_DEFAULT \
+    --format-output "target,query,tstart,tend,qstart,qend,evalue" \
+    > /dev/null
+
+echo "running mmseqs sensitive..."
+/usr/bin/time -p -o $TIME_SENSITIVE \
+    mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB_SENSITIVE $PREP \
+    --threads $THREADS \
+    -s 7.5 \
+    --max-seqs 1000 \
+    -e $E \
+    > $STDOUT
+
+awk '/real/ {print "time:", $2}' $TIME_SENSITIVE
+grep "Index table k-mer threshold:" $STDOUT
+grep "k-mer similarity threshold: " $STDOUT
+echo
+
+mmseqs convertalis $QUERY_DB $TARGET_DB $ALIGN_DB_SENSITIVE $OUT_SENSITIVE \
+    --format-output "target,query,tstart,tend,qstart,qend,evalue" \
+    > /dev/null
+
+echo "running mmseqs nail pipeline settings..."
+/usr/bin/time -p -o $TIME_NAIL \
+    mmseqs search $QUERY_DB $TARGET_DB $ALIGN_DB_NAIL $PREP \
+    --threads $THREADS \
+    -k $K_NAIL \
+    --k-score $K_SCORE_NAIL \
+    --min-ungapped-score $MIN_UNGAPPED_SCORE_NAIL \
+    --max-seqs $MAX_SEQS_NAIL \
+    -e $E \
+    > $STDOUT
+
+awk '/real/ {print "time:", $2}' $TIME_NAIL
+grep "Index table k-mer threshold:" $STDOUT
+grep "k-mer similarity threshold: " $STDOUT
+echo
+
+mmseqs convertalis $QUERY_DB $TARGET_DB $ALIGN_DB_NAIL $OUT_NAIL \
+    --format-output "target,query,tstart,tend,qstart,qend,evalue" \
+    > /dev/null
+
+sort -k7g -o $OUT_DEFAULT $OUT_DEFAULT
+sort -k7g -o $OUT_SENSITIVE $OUT_SENSITIVE
+sort -k7g -o $OUT_NAIL $OUT_NAIL
--- scripts/run-nail.sh
+++ scripts/run-nail.sh
@@ -6,6 +6,12 @@ if [ "$#" == 0 ]; then
 fi
 
 E=1e9
+THREADS=8
+
+K=6
+K_SCORE=80
+MIN_UNGAPPED_SCORE=15
+MAX_SEQS=1000
 
 BENCHMARK_DIR=$1
 NAME=$(basename "$BENCHMARK_DIR")
@@ -19,19 +25,10 @@ LONG_SEQ_TARGET_DIR=$LONG_SEQ_DIR/target/
 
 RESULTS_DIR=$BENCHMARK_DIR/results/nail/
 
-PREP_TIME_1=$RESULTS_DIR/nail.prep.1.time
-PREP_TIME_8=$RESULTS_DIR/nail.prep.8.time
-
-SEED_TIME_1=$RESULTS_DIR/nail.seed.1.time
-SEED_TIME_8=$RESULTS_DIR/nail.seed.8.time
-
-ALIGN_DEFAULT_1_TIME=$RESULTS_DIR/nail.align.1.default.time
-ALIGN_DEFAULT_8_TIME=$RESULTS_DIR/nail.align.8.default.time
-
-ALIGN_A8_B12_TIME=$RESULTS_DIR/nail.align.a8b12.time
-
+PREP_TIME=$RESULTS_DIR/nail.prep.time
+SEED_TIME=$RESULTS_DIR/nail.seed.time
+ALIGN_DEFAULT_TIME=$RESULTS_DIR/nail.align.default.time
 ALIGN_FULL_TIME=$RESULTS_DIR/nail.align.full.time
-
 ALIGN_NO_FILTERS_TIME=$RESULTS_DIR/nail.align.no-filters.time
 
 PREP=$RESULTS_DIR/prep/
@@ -40,9 +37,6 @@ SEEDS=$PREP/seeds.json
 
 OUT_DEFAULT=$RESULTS_DIR/nail.default.out
 TSV_DEFAULT=$RESULTS_DIR/nail.default.tsv
-
-OUT_A8_B12=$RESULTS_DIR/nail.a8b12.out
-TSV_A8_B12=$RESULTS_DIR/nail.a8b12.tsv
 
 OUT_FULL=$RESULTS_DIR/nail.full.out
 TSV_FULL=$RESULTS_DIR/nail.full.tsv
@@ -53,12 +47,11 @@ TSV_FULL=$RESULTS_DIR/nail.full.tsv
 OUT_NO_FILTERS=$RESULTS_DIR/nail.no-filters.out
 TSV_NO_FILTERS=$RESULTS_DIR/nail.no-filters.tsv
 
-
-
 rm -rf $RESULTS_DIR
 mkdir -p $RESULTS_DIR
 mkdir $PREP
 
+echo "running nail on long sequence pairs..."
 LONG_SEQ_TSV=$RESULTS_DIR/long-seq.tsv
 for ((i=1; i<=6; i++)); do
   LONG_QUERY="$LONG_SEQ_QUERY_DIR${i}.query.fa"
@@ -68,18 +61,67 @@ for ((i=1; i<=6; i++)); do
   rm tmp.tsv
 done
 
-/usr/bin/time -p -o $PREP_TIME_1 nail prep -t 1 --skip-hmmbuild -p $PREP $QUERY_MSA $TARGET
-/usr/bin/time -p -o $PREP_TIME_8 nail prep -t 8 --skip-hmmbuild -p $PREP $QUERY_MSA $TARGET
+echo "running nail prep..."
+/usr/bin/time -p -o $PREP_TIME \
+    nail prep \
+    -t $THREADS \
+    --skip-hmmbuild \
+    -p $PREP \
+    $QUERY_MSA $TARGET
 
-/usr/bin/time -p -o $SEED_TIME_1 nail seed -t 1 -q $QUERY_HMM -s $SEEDS $PREP
-/usr/bin/time -p -o $SEED_TIME_8 nail seed -t 8 -q $QUERY_HMM -s $SEEDS $PREP
+awk '/real/ {print "time:", $2}' $PREP_TIME
+echo
 
+echo "running nail seed..."
+/usr/bin/time -p -o $SEED_TIME \
+    nail seed \
+    -t $THREADS \
+    -q $QUERY_HMM \
+    -s $SEEDS \
+    --mmseqs_k $K \
+    --mmseqs_k_score $K_SCORE \
+    --mmseqs_min_ungapped_score $MIN_UNGAPPED_SCORE \
+    --mmseqs_max_seqs $MAX_SEQS \
+    $PREP
 
-/usr/bin/time -p -o $ALIGN_DEFAULT_1_TIME nail align -t 1 -E $E -T $TSV_DEFAULT -O $OUT_DEFAULT $QUERY_HMM $TARGET $SEEDS
-/usr/bin/time -p -o $ALIGN_DEFAULT_8_TIME nail align -t 8 -E $E -T $TSV_DEFAULT -O $OUT_DEFAULT $QUERY_HMM $TARGET $SEEDS
+awk '/real/ {print "time:", $2}' $SEED_TIME
+echo
 
-/usr/bin/time -p -o $ALIGN_A8_B12_TIME nail align -t 8 -E $E -A 8 -B 12 -T $TSV_A8_B12 -O $OUT_A8_B12 $QUERY_HMM $TARGET $SEEDS
+echo "running nail align default..."
+/usr/bin/time -p -o $ALIGN_DEFAULT_TIME \
+    nail align \
+    -t $THREADS \
+    -E $E \
+    -T $TSV_DEFAULT \
+    -O $OUT_DEFAULT \
+    $QUERY_HMM $TARGET $SEEDS
 
-/usr/bin/time -p -o $ALIGN_FULL_TIME nail align -t 8 --full-dp -E $E -T $TSV_FULL -O $OUT_FULL $QUERY_HMM $TARGET $SEEDS
+awk '/real/ {print "time:", $2}' $ALIGN_DEFAULT_TIME
+echo
 
-/usr/bin/time -p -o $ALIGN_NO_FILTERS_TIME nail align -t 8 --forward-thresh 1e9 --cloud-thresh 1e9 -E $E -T $TSV_NO_FILTERS -O $OUT_NO_FILTERS $QUERY_HMM $TARGET $SEEDS
+echo "running nail align full-dp..."
+/usr/bin/time -p -o $ALIGN_FULL_TIME \
+    nail align \
+    -t $THREADS \
+    --full-dp \
+    -E $E \
+    -T $TSV_FULL \
+    -O $OUT_FULL \
+    $QUERY_HMM $TARGET $SEEDS
+
+awk '/real/ {print "time:", $2}' $ALIGN_FULL_TIME
+echo
+
+echo "running nail align no-filters..."
+/usr/bin/time -p -o $ALIGN_NO_FILTERS_TIME \
+    nail align \
+    -t $THREADS \
+    --forward-thresh 1e9 \
+    --cloud-thresh 1e9 \
+    -E $E \
+    -T $TSV_NO_FILTERS \
+    -O $OUT_NO_FILTERS \
+    $QUERY_HMM $TARGET $SEEDS
+
+awk '/real/ {print "time:", $2}' $ALIGN_NO_FILTERS_TIME
+echo
