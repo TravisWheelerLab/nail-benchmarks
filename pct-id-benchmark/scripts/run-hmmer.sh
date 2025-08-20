64a48e83f249ff09ddb698294e1819245ba73c6b
jackroddy <jack.w.roddy@gmail.com>
2025-08-20 11:42:25 -0700

    updated run-hmmer.sh

--- pct-id-benchmark/scripts/run-hmmer.sh
+++ pct-id-benchmark/scripts/run-hmmer.sh
@@ -8,9 +8,9 @@ DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
 . $DIR/vars.sh
 set_vars "$@"
 
-O1=$RESULTS/hmmer.seq.tbl
+O1=$RESULTS/hmmer.seq.domtbl
 T1=$RESULTS/hmmer.seq.time
-O2=$RESULTS/hmmer.hmm.tbl
+O2=$RESULTS/hmmer.hmm.domtbl
 T2=$RESULTS/hmmer.hmm.time
 
 S_ARGS="--cpu $THREADS -E $E"
@@ -18,13 +18,13 @@ S_ARGS="--cpu $THREADS -E $E"
 echo "running phmmer.."
 /usr/bin/time -p -o $T1 \
     $PHMMER $S_ARGS \
-    --tblout $O1 \
+    --domtblout $O1 \
     $QUERY_FA $TARGET > /dev/null
 cat $T1 | grep real
 
 echo "running hmmsearch ..."
 /usr/bin/time -p -o $T2 \
     $HMMSEARCH $S_ARGS \
-    --tblout $O2 \
+    --domtblout $O2 \
      $QUERY_HMM $TARGET > /dev/null
 cat $T2 | grep real
