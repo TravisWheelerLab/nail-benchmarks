fa52167295015683f1655b7626edb558473d542a
jackroddy <jack.w.roddy@gmail.com>
2026-02-20 18:05:32 -0700

    updated scripts

--- pct-id-benchmark/scripts/run-nail.sh
+++ pct-id-benchmark/scripts/run-nail.sh
@@ -10,14 +10,11 @@ mkdir -p $TMP
 
 ###
 
-B=1e9
 
 QUERY=$QUERY_HMM
-run_nail "nail-s12.0-full.prf" "--mmseqs-s 12.0 --full-dp"
-run_nail "nail-s12.0-cells.prf" "--mmseqs-s 12.0 -S ${B} -C ${B} -F ${B} -E ${B}"
-run_nail "nail-s12.0.prf" "--mmseqs-s 12.0"
-# run_nail "nail-s10.0.prf" "--mmseqs-s 10.0"
-# run_nail "nail-s7.5.prf"  "--mmseqs-s 7.5"
+run_nail "nail-s12.0.prf"       "--allow-overwrite --mmseqs-s 12.0"
+# run_nail "nail-s10.0.prf"       "--allow-overwrite --mmseqs-s 10.0"
+# run_nail "nail-s7.5.prf"        "--allow-overwrite --mmseqs-s 7.5"
 
 # copy the p7hmm-derived mmseqs2 
 # profile DB to the benchmark dir
